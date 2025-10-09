c138c59c19ff9c7eb4e59f96423279b1a80309e6
Jackd44Dev <jackd44alt@hotmail.com>
2025-10-09 20:32:59 +1000

    Some bugfixes
    - Fixed lava height tracking not working properly, will now always spawn in when lava height is 10m lower than the floor's base height
    - Also fixed lava not spawning higher up if you're less than 10m away from the lava when you start the next floor
    - Added new spike model... it actually looks like a spike now!!
    - Upgraded unity version to 6000.0.59f2 so that the security vulnerability is no longer a concern :)

--- Assets/Scripts/Gameplay/LavaManager.cs
+++ Assets/Scripts/Gameplay/LavaManager.cs
@@ -5,9 +5,9 @@ public class LavaManager : MonoBehaviour
     public GameObject lavaPrefab; 
     GameObject lava; // a reference to the newly instantiated lava in this scene
     public float lavaRiseRate = 0.5f;
-    public float lavaStartHeight = -10f;
+    public float lavaStartHeight = 0f; // this might look silly, but this value will be placed somewhere between 0 and -10 a little further down in this script
     public PlayerData playerData;
-    bool lavaSpawned = false;
+    bool lavaSpawned = false; // tracks whether the lava has been spawned in this scene yet
 
     void Start()
     {
@@ -18,9 +18,7 @@ public class LavaManager : MonoBehaviour
     {
         if (lavaSpawned)
         {
-            float lavaHeight = lava.transform.position.y + (lavaRiseRate * Time.deltaTime); // calculate the lava's new height
-            calculateLavaHeight(lavaHeight); // add the lava's height to playerData, comparing the old height to the float above (so this line CANNOT be below the next line)
-            lava.transform.position = new Vector3(0, lavaHeight, 0); // slowly rise the lava's in-world position
+            raiseLava();
         }
         else // if lava hasn't spawned yet because it's too far away, its height increase is simulated (at a faster 1.5x rate!) until it is close enough to be spawned
         {
@@ -32,20 +30,24 @@ public class LavaManager : MonoBehaviour
     void checkToSpawnLava()
     {
         if (lavaSpawned) { return; }
-        if (playerData.baseHeight <= (10 + playerData.lavaHeight)) // if the lava is 10m or closer to the floor's base height, spawn the lava
+        if (playerData.baseHeight <= (10 + playerData.lavaHeight)) // if the lava is 10m or closer to the floor's *base* height (not the player's current height!), spawn the lava
         {
             lavaSpawned = true;
-            float physicalLavaOffset = Mathf.Clamp(10 - (playerData.height - playerData.lavaHeight), 0, 10); // if lava height is less than 10 height away from the player, spawn it higher up, closer to the player 
-            lavaStartHeight += physicalLavaOffset; // add the offset to the starting height
-            Vector3 lavaSpawnPosition = new Vector3(0, lavaStartHeight, 0); // set lava at its proper start height
+            float lavaHeightOffset = Mathf.Clamp(playerData.baseHeight - playerData.lavaHeight, 0, 10); // clamps the distance between the base height and the lava, higher value means the lava spawns lower! 
+            Debug.Log("Player height is: " + playerData.baseHeight + "m, Lava height is: " + playerData.lavaHeight + "m, Height difference is: " + (playerData.baseHeight - playerData.lavaHeight) + "m");
+            Debug.Log("Offsetting lava to be -" + lavaHeightOffset + "m!");
+            lavaStartHeight -= lavaHeightOffset; // the lava will spawn at y = 0 by default, so this line will instead make the lava spawn further down from the player, based on lavaHeightOffset
+            Vector3 lavaSpawnPosition = new Vector3(0, lavaStartHeight, 0); // set lava at its adjusted start height
             lava = Instantiate(lavaPrefab, lavaSpawnPosition, Quaternion.identity);
         }
     }
 
-    void calculateLavaHeight(float newTargetLavaHeight) // increases playerData's tracked lavaHeight as the lava physically moves up in the world
+    void raiseLava()
     {
-        float lavaHeightIncrease = newTargetLavaHeight - lava.transform.position.y;
-        playerData.lavaHeight += lavaHeightIncrease;
+        float amountToRaiseBy = (lavaRiseRate * Time.deltaTime);
+        float lavaHeight = lava.transform.position.y + amountToRaiseBy; // calculate the lava's new height
+        playerData.lavaHeight += amountToRaiseBy; // increase the playerData lava tracker to keep it in sync
+        lava.transform.position = new Vector3(0, lavaHeight, 0); // slowly rise the lava's in-world position
     }
 
     void simulateLavaHeight() // increases lavaHeight if the lava is not physically present in the world
